3d04dbfc566659ad72add25d64c24a4c9b2b495a
Mike Dodd <mike@wilxite.com>
2019-08-06 11:52:13 +0100

    Updated home page

--- index.php
+++ index.php
@@ -18,17 +18,30 @@
 
         </nav>
 
-        <h1>Header 1</h1>
-        <h2>Header 2</h2>
-        <h3>Header 3</h3>
-        <h4>Header 4</h4>
-        <h5>Header 5</h5>
-        <h6>Header 6</h6>
+        <h1>Welcome to Mikes Default System Styles</h1>
 
-        <h2>HTML 5 Elements</h2>
+        <div class="containerSplit halfContainer">
 
-        <h3>Meter</h3>
-        <meter value="0.4"></meter>
+            <div class="container">
+
+                <h2>About</h2>
+
+                <p>These default styles are designed for systems rather than website, this can include software such as CMSs, CRMs or Anything you want to use it for</p>
+
+                <h2>Licence</h2>
+
+                <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+
+            </div>
+
+            <div class="container">
+
+                <h2>Latest Release</h2>
+
+            </div>
+
+
+        </div>
 
     </body>
 </html>
